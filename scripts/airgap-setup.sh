e330106451ca1a06f5f08489c9d3d052daf2bb38
btbf <b.t.b.f.corp@gmail.com>
2025-10-26 07:16:34 +0100

    fixed airgap get node version

--- scripts/airgap-setup.sh
+++ scripts/airgap-setup.sh
@@ -72,7 +72,7 @@ Main(){
     echo -e "${GREEN}依存関係のインストールが完了しました${NC}"
 
     #cardano-cliインストール
-    source <(curl -fsSL https://raw.githubusercontent.com/btbf/sjg-tools/refs/heads/main/scripts/spokit.library)
+    source <(curl -fsSL https://raw.githubusercontent.com/btbf/sjg-tools/refs/heads/main/scripts/components/node_install)
 
     echo "アーキテクチャ判定中..."
     arch=$(uname -m)
